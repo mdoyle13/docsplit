08de3cd71138f0c3c3c23ac453448cc6826e72d9
Mike Doyle <mdoyle13@gmail.com>
2018-09-12 16:14:24 -0500

    Update pdf_extractor.rb

--- lib/docsplit/pdf_extractor.rb
+++ lib/docsplit/pdf_extractor.rb
@@ -126,6 +126,7 @@ module Docsplit
           `gm convert #{escaped_doc} #{escaped_out}/#{escaped_basename}.pdf`
         else
           if libre_office?
+            throw Libre
             # Set the LibreOffice user profile, so that parallel uses of cloudcrowd don't trip over each other.
             ENV['SYSUSERCONFIG']="file://#{File.expand_path(escaped_out)}"
             
